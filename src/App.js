1ba96e87f0567ca1c0528e0d69984ce5658a3b5a
Pratikkaltana <Pratikkatlana7@gmail.com>
2023-03-08 11:26:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetails from './EmpDetails';
+import EmpEdit from  './EmpEdit';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React CRUD</h2>
+      <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<EmpListing />}></Route>
+    <Route path='/employee/create' element={<EmpCreate />}></Route>
+    <Route path='employee/details/:empid' element={<EmpDetails />}></Route>
+    <Route path='employee/edit/:empid' element={<EmpEdit />}></Route>
+  </Routes>
+  </BrowserRouter>
+    </div>
+  );
+  }
+
+export default App;
